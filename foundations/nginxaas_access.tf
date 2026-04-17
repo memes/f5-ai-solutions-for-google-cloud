dadf1bc226a25d50d22ff87db051dd38f3acfd70
Matthew Emes <memes@matthewemes.com>
2026-04-16 18:35:56 -0700

    fix: Add port 80 to NGINXaaS attachment

--- foundations/nginxaas_access.tf
+++ foundations/nginxaas_access.tf
@@ -6,19 +6,12 @@ module "nginxaas" {
     pool_id = var.workload_identity_pool_id
     name    = format("%s-nginxaas", var.name)
   }
-  attachments = merge({
-    for k, v in local.regional_names : format("%s-https", k) => {
-      subnet             = module.vpc.subnets_by_region[k].self_link
-      port               = 443
-      service_attachment = try(var.nginxaas.attachments[k].service_attachment, null)
+  attachments = { for k, v in local.regional_names : k => {
+    subnet             = module.vpc.subnets_by_region[k].self_link
+    ports              = [80, 443]
+    service_attachment = try(var.nginxaas.attachments[k].service_attachment, null)
     }
-    }, {
-    for k, v in local.regional_names : format("%s-http", k) => {
-      subnet             = module.vpc.subnets_by_region[k].self_link
-      port               = 80
-      service_attachment = try(var.nginxaas.attachments[k].service_attachment, null)
-    }
-  })
+  }
   service_accounts = try(var.nginxaas.service_accounts, null)
   secrets          = try(var.nginxaas.secrets, null)
 }
