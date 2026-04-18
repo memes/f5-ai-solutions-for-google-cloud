3860aa768daa797f2edee8b754b036edaa1470f2
Matthew Emes <memes@matthewemes.com>
2026-04-17 17:31:37 -0700

    fix: One backend service per NEG for PSC

--- foundations/nginxaas_access.tf
+++ foundations/nginxaas_access.tf
@@ -6,7 +6,7 @@ module "nginxaas" {
     pool_id = var.workload_identity_pool_id
     name    = format("%s-nginxaas", var.name)
   }
-  attachments = { for k, v in local.regional_names : k => {
+  attachments = { for k, v in local.regional_names : k => { # TODO(@memes): This should be v
     subnet             = module.vpc.subnets_by_region[k].self_link
     ports              = [80, 443]
     service_attachment = try(var.nginxaas.attachments[k], null)
@@ -17,21 +17,18 @@ module "nginxaas" {
 }
 
 resource "google_compute_region_backend_service" "nginxaas" {
-  for_each              = var.nginxaas == null ? {} : try(module.nginxaas["enabled"].network_endpoint_groups_by_region, {})
+  for_each              = var.nginxaas == null ? {} : try(module.nginxaas["enabled"].network_endpoint_groups_by_name, {})
   project               = var.project_id
-  name                  = local.regional_names[each.key]
-  region                = each.key
+  name                  = each.key
+  region                = reverse(split("/", each.value))[2]
   load_balancing_scheme = "EXTERNAL_MANAGED"
   locality_lb_policy    = "MAGLEV"
   protocol              = "TCP"
-  dynamic "backend" {
-    for_each = each.value
-    content {
-      balancing_mode               = "CONNECTION"
-      capacity_scaler              = 1.0
-      max_connections_per_endpoint = 1000
-      group                        = backend.value
-    }
+  backend {
+    balancing_mode               = "CONNECTION"
+    capacity_scaler              = 1.0
+    max_connections_per_endpoint = 1000
+    group                        = each.value
   }
 
   depends_on = [
@@ -40,7 +37,7 @@ resource "google_compute_region_backend_service" "nginxaas" {
 }
 
 resource "google_compute_forwarding_rule" "http" {
-  for_each              = google_compute_region_backend_service.nginxaas
+  for_each              = { for k, v in google_compute_region_backend_service.nginxaas : k => v if endswith(k, "-80") }
   project               = var.project_id
   name                  = local.regional_names[each.key]
   description           = "Send HTTP traffic to NGINXaaS."
@@ -58,7 +55,7 @@ resource "google_compute_forwarding_rule" "http" {
 }
 
 resource "google_compute_forwarding_rule" "https" {
-  for_each              = google_compute_region_backend_service.nginxaas
+  for_each              = { for k, v in google_compute_region_backend_service.nginxaas : k => v if endswith(k, "-443") }
   project               = var.project_id
   name                  = local.regional_names[each.key]
   description           = "Send HTTPS traffic to NGINXaaS."
